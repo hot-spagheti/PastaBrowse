e629fe098fc0511931d23c4ef23128a925be7c45
notspagheti <notspagheti@gmail.com>
2026-04-07 21:21:45 +0200

    Cleanup:  Rebound searchBtn to intended function

--- public/client_main.js
+++ public/client_main.js
@@ -1,9 +1,9 @@
-import {loadURL, saveNav, refresh, history_backward, history_forward, tab_list} from "./navigation.js";
+import {loadURL, saveNav, refresh, history_backward, history_forward} from "./navigation.js";
 import {newTab, removeTab, setTitleAndFavIcon, switchTab, loadLastSesh} from "./tabs.js";
 import {root_exit, toggleMaximize, minimize, onCtrlT, onCtrlW, getHistory, onResHistory} from "./ipc.js";
 
 const searchBtn = document.getElementById("searchBtn");
-searchBtn.addEventListener("click", () => {console.log(tab_list)});
+searchBtn.addEventListener("click", loadURL);
 
 const input = document.getElementById("url");
 input.addEventListener("keypress", (e) => {

cca9bda4d489b73d28bcd024deca8be177240ec0
notspagheti <notspagheti@gmail.com>
2026-04-16 16:28:01 +0200

    Add: text, exit & border vars for themes

--- public/settings_page/settings_main.js
+++ public/settings_page/settings_main.js
@@ -9,3 +9,24 @@ greenBtn.addEventListener("click", () => {setTheme("green")});
 
 const blueBtn = document.getElementById("blueBtn");
 blueBtn.addEventListener("click", () => {setTheme("blue")});
+
+const orangeBtn = document.getElementById("orangeBtn");
+orangeBtn.addEventListener("click", () => {setTheme("orange")});
+
+const amberBtn = document.getElementById("amberBtn");
+amberBtn.addEventListener("click", () => {setTheme("amber")});
+
+const tealBtn = document.getElementById("tealBtn");
+tealBtn.addEventListener("click", () => {setTheme("teal")});
+
+const indigoBtn = document.getElementById("indigoBtn");
+indigoBtn.addEventListener("click", () => {setTheme("indigo")});
+
+const purpleBtn = document.getElementById("purpleBtn");
+purpleBtn.addEventListener("click", () => {setTheme("purple")});
+
+const pinkBtn = document.getElementById("pinkBtn");
+pinkBtn.addEventListener("click", () => {setTheme("pink")});
+
+const greyBtn = document.getElementById("greyBtn");
+greyBtn.addEventListener("click", () => {setTheme("grey")});
